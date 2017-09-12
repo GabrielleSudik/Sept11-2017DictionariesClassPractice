c0b687e69c11212eec177e1e890de633d7910a1a
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-09-11 20:38:35 -0400

    initial commit

---
+++ Sept11-2017DictionariesClassPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept11_2017DictionariesClassPractice
+{
+    class Program
+    { 
+        //DICTIONARIES
+        //data collection of key/value pairs
+        //the key is always unique (think of a real dictionary) -- each key is only one
+        //the value is not always unique (think of different words that mean the same thing)
+        //another analogy: coat check ticket stubs are all different
+        //but there might be 10 identical coats in the closet
+    
+        static void Main(string[] args)
+        {
+        }
+    }
+}
